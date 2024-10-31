f0f2f480a35f124bce11b7e4b6a99cf2dcd7572d
frivolous-impulsor <ultrapilot95@gmail.com>
2024-10-31 09:13:12 -0400

    assignment2 completed and tested

--- assignmentProj/src/a2q3_li2177.hs
+++ assignmentProj/src/a2q3_li2177.hs
@@ -1,8 +1,12 @@
+{-# LANGUAGE GADTs #-}
+
 data Expr a where
     ExBool :: Bool -> Expr Bool
     ExInt :: Int -> Expr Int
     ExPlus :: Expr Int -> Expr Int -> Expr Int
     ExMinus :: Expr Int -> Expr Int -> Expr Int
+    ExTimes :: Expr Int -> Expr Int -> Expr Int
+    ExPower :: Expr Int -> Expr Int -> Expr Int
     ExLessThan :: Expr Int -> Expr Int ->  Expr Bool
     ExGreaterThan :: Expr Int -> Expr Int ->  Expr Bool
     ExEquals :: Expr Int -> Expr Int ->  Expr Bool
@@ -13,6 +17,8 @@ eval e = case e of
     ExInt a -> a
     ExPlus a b -> (eval a) + (eval b)
     ExMinus a b -> (eval a) - (eval b)
+    ExTimes a b -> (eval a) * (eval b)
+    ExPower a b -> (eval a) ^ (eval b)  --for integer power only
     ExLessThan a b -> (eval a) < (eval b)
-    ExGreaterThan a b -> (eval a) < (eval b)
+    ExGreaterThan a b -> (eval a) > (eval b)
     ExEquals a b -> (eval a) == (eval b)
